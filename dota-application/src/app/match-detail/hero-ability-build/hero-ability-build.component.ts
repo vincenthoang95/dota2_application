8a37690bd5c8862f2c06c6c98cde34cccd5b87f5
Vincent Hoang <vincenthoang95@gmail.com>
2020-04-06 19:49:00 -0500

    working on displaying ability builds

--- dota-application/src/app/match-detail/hero-ability-build/hero-ability-build.component.ts
+++ dota-application/src/app/match-detail/hero-ability-build/hero-ability-build.component.ts
@@ -1,21 +1,24 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ChangeDetectionStrategy } from '@angular/core';
 import { HeroAbilityBuildService } from './hero-ability-build.service';
 
 @Component({
   selector: 'app-hero-ability-build',
   templateUrl: './hero-ability-build.component.html',
-  styleUrls: ['./hero-ability-build.component.css']
+  styleUrls: ['./hero-ability-build.component.css'],
+  // changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class HeroAbilityBuildComponent implements OnInit {
 
   @Input() teamPlayers;
   @Input() heroInfo;
   
-  abilityIdList;
-  abilityList;
+  abilityIdList:any={};
+  abilityList:any={};
 
   totalLevels = 25;
 
+  initDone=false;
+  initDone2=false;
 
 
   constructor(
@@ -24,14 +27,24 @@ export class HeroAbilityBuildComponent implements OnInit {
 
   ngOnInit() {
     this.heroAbilityBuildService.getAbilityIds().subscribe(
-      data=>this.abilityIdList=data,
+      data=>{
+        this.abilityIdList=data
+        this.initDone=true
+        // console.log("this is data ",data);
+      },
+      
+      error=>console.log(error)
+      )
+      this.heroAbilityBuildService.getAbilityList().subscribe(
+        data=>{this.abilityList=data
+          this.initDone2=true
+    
+      // console.log("this is data2 ",data);
+    },
       error=>console.log(error)
     )
 
-    this.heroAbilityBuildService.getAbilityList().subscribe(
-      data=>this.abilityList=data,
-      error=>console.log(error)
-    )
+
   }
 
   totalLevelList(){
@@ -47,6 +60,62 @@ export class HeroAbilityBuildComponent implements OnInit {
     var imgName = img.split("/").pop().slice(0,-1);
     return imgName;
   }
+
+  getAbilityImage(abilityId):any{
+
+
+    // console.log(abilityId);
+    if(this.abilityIdList === undefined){
+      console.log("abilityIdList is undefined");
+    }
+    if(this.abilityList === undefined){
+      console.log("abilityList is undefined");
+    }
+    if(abilityId === undefined){
+      return "";
+    }
+    var abilityName = this.abilityIdList[abilityId];
+    // console.log("this asdkjklas ",this.abilityList)
+    if (abilityName.split("_")[0] == "special"){
+      return "talent.png";
+    }
+    else{
+      // console.log(this.abilityList);
+      var abilityImg = this.abilityList[abilityName]["img"].split("/").pop();
+      return abilityImg;
+    }
+  }
+
+  sortAbilities(abilityArray){
+    // var newArray = new Array();
+    // for(var abilityId of abilityArray ){
+    //   newArray.push(abilityId);
+    // }
+    // console.log(newArray);
+
+    var reverseList = abilityArray.reverse();
+    
+    var newArray = new Array();
+    var level = 0;
+    while(level < 25){
+
+      // if(reverseList.length === 0){
+      //   break;
+      // }
+
+      if([17,19,21,22,23,24].indexOf(level+1) !== -1){
+        newArray.push(undefined);
+      }
+      else{
+        newArray.push(reverseList.pop());
+      }
+      level++;
+    }
+    return newArray;
+    // console.log(newArray)
+  }
+
+  
 
 }
 
