ed87eec7664b2b5e917aaeec3dade40d455cf0a8
Vincent Hoang <vincenthoang95@gmail.com>
2020-04-28 15:55:09 -0500

    get filtered data

---
+++ dota_app/src/main/java/com/dota/controller/PlayerController.java
@@ -0,0 +1,38 @@
+package com.dota.controller;
+
+import java.util.Optional;
+
+import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.dota.utils.OpenDotaAPIUtils;
+
+@RestController
+@CrossOrigin
+public class PlayerController {
+	
+	
+	@Autowired
+	OpenDotaAPIUtils openDotaAPIUtils;
+	
+	@GetMapping("/players/{accountId}/heroes")
+	public String filterPlayerHeroesPlayed(@PathVariable long accountId, 
+			@RequestParam(required = false) Optional<Integer> date,
+			@RequestParam(required = false) Optional<Integer> patch,
+			@RequestParam(required = false) Optional<Integer> region		
+			) {
+		
+		
+		return openDotaAPIUtils.filterPlayerHeroesPlayed(accountId, date.orElse(0), patch.orElse(0), region.orElse(0));
+		
+//		JSONObject playerResponse = new JSONObject(openDotaAPIUtils.filterPlayerHeroesPlayed(accountId, date, patch, region));
+//		return "";
+	}
+	
+	
+}
--- dota_app/src/main/java/com/dota/utils/OpenDotaAPIUtils.java
+++ dota_app/src/main/java/com/dota/utils/OpenDotaAPIUtils.java
@@ -1,11 +1,13 @@
 package com.dota.utils;
 
+import org.json.JSONObject;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 @Service
 public class OpenDotaAPIUtils {
 	//this is a random test
@@ -22,6 +24,39 @@ public class OpenDotaAPIUtils {
 		RestTemplate restTemplate = new RestTemplate();
 		ResponseEntity<String> response = restTemplate.exchange("https://api.opendota.com/api/matches/" + matchId,HttpMethod.GET,dotaOpenAPIHeader(),String.class);
 		return response.getBody();
+	}
+	
+	public String filterPlayerHeroesPlayed(long playerId, Integer date, Integer patch, Integer region) {
+		
+		
+//		JSONObject jo = new JSONObject();
+//		if(date != 0) jo.put("date", date);
+//		if(patch != 0) jo.put("patch",patch);
+//		if(region != 0) jo.put("region", region);
+		
+		String filterPath = "";
+
+		if(date != 0) filterPath += "date=" + date + "&";
+		if(patch != 0) filterPath += "patch=" + patch + "&";
+		if(region != 0) filterPath += "region=" +region + "&";
+		System.out.println(filterPath);
+		System.out.println("https://api.opendota.com/api/players/" + playerId + "/heroes?" + filterPath);
+		// my player id
+		playerId = 93102567;
+		String url = "https://api.opendota.com/api/players/" + playerId + "/heroes?" + filterPath;
+//		
+//		return url;
+		
+//		UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl("https://api.opendota.com/api/players/" + playerId + "/heroes?" + filterPath).encode();
+
+//		UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl("https://api.opendota.com/api/players/" + playerId + "/heroes")
+//				.queryParams(jo);
+		
+//		return builder.toUriString();
+		
+		RestTemplate restTemplate = new RestTemplate();
+		ResponseEntity<String> response = restTemplate.exchange(url,HttpMethod.GET,dotaOpenAPIHeader(),String.class);
+		return response.getBody();		
 	}
 	
 }
