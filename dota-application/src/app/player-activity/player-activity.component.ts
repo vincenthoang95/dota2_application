4b2738142ef5c78172b5e1df04a3c9379fea554c
Vincent Hoang <vincenthoang95@gmail.com>
2020-05-06 17:38:53 -0500

    displays date layout

--- dota-application/src/app/player-activity/player-activity.component.ts
+++ dota-application/src/app/player-activity/player-activity.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ConstantsService } from '../common-services/constants.service';
+import { range } from 'rxjs';
+import { MapType } from '@angular/compiler';
 
 @Component({
   selector: 'app-player-activity',
@@ -8,13 +10,18 @@ import { ConstantsService } from '../common-services/constants.service';
 })
 export class PlayerActivityComponent implements OnInit {
 
+  begin2020 = new Date(1577836800 * 1000);
+  date = new Date();
+
 
   // time = 1578207559;
   time = 1328400078;
 
-  color = "rgb(177.5,177.5,0)";
-
   allPlayerMatches;
+
+  weekLayout = [];
+  // weekLayout = {};
+  weekLayout2d = [];
 
   constructor(private constantService:ConstantsService) { }
 
@@ -35,8 +42,24 @@ export class PlayerActivityComponent implements OnInit {
               if(date.getFullYear() == matchDate.getFullYear())
                 dateMatches.push(match);
         }
-        console.log(data);
+        // console.log(data);
         console.log(dateMatches);
+
+        let sortByYear = new Map();
+        let startYear = 2012;
+        
+        for(let year = startYear; year <= this.date.getFullYear(); year++){
+          let matchesList = []
+          for(let match of this.allPlayerMatches){
+            let matchDate = new Date(match.start_time * 1000);
+            if(matchDate.getFullYear() == year){
+              matchesList.push(match);
+            }
+          }
+          sortByYear.set(year, matchesList);
+        }
+        console.log(sortByYear.get(2012));
+
       },
       error => console.log(error)
     );
@@ -47,7 +70,66 @@ export class PlayerActivityComponent implements OnInit {
     let newDate = new Date(tempDate.setDate(tempDate.getDate() + 1));
     console.log('after    ' + newDate);
 
+    let testDate = new Date();
+    testDate.setFullYear(2020);
+    testDate.setMonth(0);
+    testDate.setDate(1);
+    console.log(testDate);
 
+
+    let weekList = [];
+    let weekNum = 1;
+    while(true){
+      if(testDate.getFullYear() == 2021){
+        // this.weekLayout[weekNum] = weekList;
+        this.weekLayout.push({weekNum:weekList});
+        this.weekLayout2d.push(weekList);
+        break;
+      }
+
+      switch(testDate.getDay()){
+        case 0:
+        case 1:
+        case 2:
+        case 3:
+        case 4:
+        case 5:
+          weekList.push(testDate.toString());
+          break;
+        case 6:
+          weekList.push(testDate.toString());
+          // this.weekLayout[weekNum] = weekList;
+          this.weekLayout.push({weekNum:weekList});
+          this.weekLayout2d.push(weekList);
+          weekNum++;
+          weekList = [];
+          break;
+      }
+      
+      if(testDate.getFullYear() == this.date.getFullYear() && testDate.getMonth() == this.date.getMonth() && testDate.getDay() == this.date.getDay()){
+        let weekListSize = weekList.length;
+        if(weekListSize != 7){
+          while(weekListSize != 7){
+            weekList.push(0);
+            weekListSize++;
+          }
+        // this.weekLayout[weekNum] = weekList;
+        this.weekLayout.push({weekNum:weekList});
+        this.weekLayout2d.push(weekList);
+        }
+
+        break;
+      }
+
+
+      testDate.setDate(testDate.getDate()+1);
+
+    }
+    console.log(this.weekLayout)
+
+    // for(let i of this.weekLayout){
+    //   console.log(i);
+    // }
 
   }
 
@@ -75,5 +157,12 @@ export class PlayerActivityComponent implements OnInit {
   getRadius(){
     return 7;
   }
+
+
+  displayDay(date){
+    if(date === 0) return 'blank';
+    return new Date(date).toLocaleDateString();
+  }
+
 
 }
