707c20125f115ad896277377ce6c7687a64f9b58
Vincent Hoang <vincenthoang95@gmail.com>
2020-05-28 15:22:53 -0500

    adding constants back

---
+++ dota-application/src/app/config/constants.ts
@@ -0,0 +1,6 @@
+import { Injectable } from '@angular/core';
+@Injectable()
+export class Constants {
+    // public URL: string = 'http://localhost:8080';
+    public URL: string = 'http://dotaappbackend-env.eba-tvxiizmf.us-east-2.elasticbeanstalk.com';
+}
